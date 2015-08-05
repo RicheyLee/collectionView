d3ef0cd1a21512bdfcbd8de5a58eea9bcbc26249
Richey <liqi1234@foxmail.com>
2015-08-05 17:41:02 +0800

    Initial Commit

---
+++ collectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectDemo
+//
+//  Created by Richey on 15/8/5.
+//  Copyright (c) 2015年 kanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
